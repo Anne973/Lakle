4259ecf2297a2d2f559a9fce9a9cd5f3132a9d0e
Anne <anne.derenoncourt@gmail.com>
2018-01-07 20:19:53 -0300

    debug liip + twigs propriétaires et locataires + pagination articles + formulaire contact

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\ContactType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -27,7 +28,7 @@ class HomeController extends Controller
                 0                            // Offset
             );
 
-        return $this->render('Home/index.html.twig', array('lastArticles'=>$lastArticles));
+        return $this->render('Home/index.html.twig', array('lastArticles' => $lastArticles));
 
     }
 
@@ -44,10 +45,56 @@ class HomeController extends Controller
     /**
      * @Route("/contact", name="contact")
      */
-    public function contactAction()
+    public function contactAction(Request $request)
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
 
-        return $this->render('Home/contact.html.twig');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $message = (new \Swift_Message('Contact'))
+                ->setFrom($data['email'])
+                ->setTo($this->getParameter('mailer_user'))
+                ->setBody(
+                    $this->renderView(
 
+                        'Emails/contact.html.twig',
+                        array('data' => $data)
+
+                    ),
+                    'text/html'
+                );
+            $this->get('mailer')->send($message);
+            return $this->redirectToRoute('confirmation');
+        }
+        return $this->render('Home/contact.html.twig', array('form'=>$form->createView()));
+
+    }
+
+    /**
+     * @Route("/confirmation", name="confirmation")
+     */
+    public function confirmationAction()
+    {
+        return $this->render('Home/confirmation.html.twig');
+    }
+
+
+
+    /**
+     * @Route("/proprietaires", name="proprietaires")
+     */
+    public function ownersAction()
+    {
+        return $this->render('Home/owners.html.twig');
+    }
+
+    /**
+     * @Route("/locataires", name="locataires")
+     */
+
+    public function tenantsAction()
+    {
+        return $this->render('Home/tenants.html.twig');
     }
 }
